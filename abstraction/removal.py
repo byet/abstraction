87fb55962f5377d26784b21b57f581a9da117e49
Barbaros Yet <barbarosyet@gmail.com>
2023-03-11 14:57:27 +0300

    Removal of observed nodes
    * bug fix reverse_edge, when checking children's parents, skips the reversed edge
    * bug fix erase_node raises exception if the arcs cannot be reversed without introducing a cycle
    * adds erase_observed_nodes, makes adds reversals to make the node parentless
    * adds observed_nodes feature to remove_minimal_node and remove_minimal_nodes in abstraction class

--- abstraction/removal.py
+++ abstraction/removal.py
@@ -6,37 +6,51 @@ class Abstraction:
     def __init__(self) -> None:
         pass
 
-    def minimal_remove_node(self, dag, node):
+    def minimal_remove_node(self, dag, node, observed=False):
         """Removes a node by introducing minimal number of additional arcs.
 
         Args:
             dag (Dag): DAG object
             node (str): node name
+            observed (bool): True if node is assumed to be conditioned on
 
         Returns:
             Dag, int: returns both the simplified DAG, and score (number of additional edges)
         """
-        children = dag.successors(node)
-        reverse_orders = permutations(children)
+        if observed: 
+            arcs_to_reverse = dag.predecessors(node)
+        else:
+            arcs_to_reverse = dag.successors(node)
+            
+        reverse_orders = permutations(arcs_to_reverse)
         best_score = float("inf")
         best_order = []
         G = Dag()
         for order in reverse_orders:
             graph = dag.copy()
-            score = graph.erase_node(node, reverse_order=order)
-            print(score)
+            if observed:
+                try:
+                    score = graph.erase_observed_node(node, reverse_order=order)
+                except:
+                    continue
+            else:
+                try: 
+                    score = graph.erase_node(node, reverse_order=order)
+                except:
+                    continue
             if score < best_score:
                 G = graph.copy()
                 best_score = score
                 best_order = order
         return G, best_order, best_score
 
-    def minimal_remove_nodes(self, dag, nodes):
+    def minimal_remove_nodes(self, dag, nodes, observed_nodes=[]):
         """Removes a set of nodes node by introducing minimal number of additional arcs.
 
         Args:
             dag (Dag): DAG object
             nodes (list): list of node names to be removed.
+            observed_nodes (list): list of node names that are observed
 
         Returns:
             Dag, order int, int: Abstracted dag object, list of removed nodes, maximum number of parents in the dag, the total number of parents in the dag
@@ -49,7 +63,8 @@ class Abstraction:
         for order in remove_orders:
             graph = dag.copy()
             for node in order:
-                graph, score = self.minimal_remove_node(graph, node)
+                observed = node in observed_nodes
+                graph, _, score = self.minimal_remove_node(graph, node, observed)
             # Scores
             num_parents = dict(dag.in_degree()).values()
             score1 = max(num_parents)
--- issues.md
+++ issues.md
@@ -44,3 +44,50 @@ The algorithm would be like try to put back MB and remove edges due to removing 
 Should I think in terms of node arc reversal or node removal?
 
 For this you have to keep track of all assumptions starting from the initial model... E.g. if you there is a causal relation at th ebeginning and if you added causal arcs at each abstraction, you have to keep track of them...
+
+We assume all nodes are unobserved. We also need the abstraction operations for conditioned nodes (which works the opposite,  you want to leave it parentless or with one parent)
+
+BUGS!
+
+Reversal operations
+
+edges = [(1,3),(2,3),(3,4),(3,5), (1,4), (6,5)]
+dag = Dag(edges)
+abs = Abstraction()
+g,order, _, _ = abs.minimal_remove_nodes(dag, [3,5,1], [5])
+g.edges()
+
+
+Output exceeds the size limit. Open the full output data in a text editor
+---------------------------------------------------------------------------
+KeyError                                  Traceback (most recent call last)
+File ~/anaconda3/envs/abstraction/lib/python3.11/site-packages/networkx/classes/digraph.py:899, in DiGraph.successors(self, n)
+    898 try:
+--> 899     return iter(self._succ[n])
+    900 except KeyError as err:
+
+KeyError: 1
+
+The above exception was the direct cause of the following exception:
+
+NetworkXError                             Traceback (most recent call last)
+Cell In[26], line 4
+      2 dag = Dag(edges)
+      3 abs = Abstraction()
+----> 4 g,order, _, _ = abs.minimal_remove_nodes(dag, [3,5,1], [5])
+      5 g.edges()
+
+File ~/Documents/abstraction/abstraction/removal.py:63, in Abstraction.minimal_remove_nodes(self, dag, nodes, observed_nodes)
+     61 for node in order:
+     62     observed = node in observed_nodes
+---> 63     graph, _, score = self.minimal_remove_node(graph, node, observed)
+     64 # Scores
+     65 num_parents = dict(dag.in_degree()).values()
+...
+    899     return iter(self._succ[n])
+    900 except KeyError as err:
+--> 901     raise NetworkXError(f"The node {n} is not in the digraph.") from err
+
+NetworkXError: The node 1 is not in the digraph.
+
+Seems not possible...
